9590db9d84c93b175bef8d5cba4292ca39b57c8a
Gabriel Lira <gabriel.tertulino@proton.me>
2024-06-18 11:06:51 -0300

    v6.0

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,4 +1,18 @@
 const form = document.querySelector('.hero__form');
 console.log(form);
-const emailInput = document.getElementById('#email');
+const errorMsg = document.querySelector('.input__error');
+console.log(errorMsg);
+const emailInput = document.getElementById('email');
 console.log(emailInput);
+
+function showError() {
+  errorMsg.textContent = "Valid email required";
+}
+
+
+form.addEventListener('submit', (event) => {
+  if (!emailInput.validity.valid) {
+    event.preventDefault();
+    showError();
+  }
+})
